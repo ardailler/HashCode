46a7ddd25589106d401345fb29488887ca760124
Thibaud Perrin <thibaudperrin@outlook.com>
2021-04-04 21:21:06 +0200

    Update environment

--- read_hash_code.py
+++ read_hash_code.py
@@ -5,6 +5,9 @@ class Vehicle:
         self.index = 0 # Position in the streets path
         self.score = 0 # the score of the vehicle
 
+    def __repr__(self):
+        return str(f"nb_streets={self.nb_streets} streets=[{', '.join(self.streets)}]")
+
     def print(self):
         print(f"nb_streets={self.nb_streets} streets=[{', '.join(self.streets)}]")
 
@@ -17,16 +20,31 @@ class Street:
         self.T = time # Time to travel across the street
         self.vehicles = [] # tuple list of vehicle and the time when they arrive at the end of street (vehicle, 2)
 
+    def __repr__(self):
+        return str(f"id_b={self.id_b} id_e={self.id_e} name={self.name} T={self.T}")
+
     def print(self):
         print(f"id_b={self.id_b} id_e={self.id_e} name={self.name} T={self.T}")
     
 class Intersection:
-    def ___init___(self, id, streets_i, streets_o, schedulers):
-        self.id = id
-        self.streets_i = streets_i # List of incomming streets
-        self.streets_o = streets_o # List of outgoing streets
-        self.schedulers = schedulers # List of tuple (incomming street, duration of green light)
+    def __init__(self, id_i):
+        self.id = id_i
+        self.streets_i = [] # List of incomming streets
+        self.streets_o = [] # List of outgoing streets
+        self.schedulers = [] # List of tuple (incomming street, duration of green light)
         self.index = 0 # index of scheduler
+    
+    def addStreetI(self, street):
+        self.streets_i.append(street)
+
+    def addStreetO(self, street):
+        self.streets_o.append(street)
+
+    def __repr__(self):
+        return str(f"\nID={self.id} \n\t streets_i={self.streets_i} \n\t streets_o={self.streets_o}\n\n----------\n")
+
+    def print(self):
+        print(f"\nID={self.id} \n\t streets_i={self.streets_i} \n\t streets_o={self.streets_o}\n\n----------\n")
 
 '''
 to display node graph
@@ -41,7 +59,7 @@ class Env:
         self.F = 0 # Score for each vehicles (when endding their path)
 
         self.streets = []
-        self.intersections = []
+        self.intersections = {}
         self.vehicles = []
 
         self.visited_streets = []
@@ -52,6 +70,19 @@ class Env:
         self.S = s
         self.V = v
         self.F = f
+
+    def addOrCreateIntersection(self, id_i, street, inOrOut):
+        if id_i in self.intersections: # the case where the intersection already exist
+            intersection = self.intersections[id_i]
+        else: # Case where the intersection not exist
+            intersection = Intersection(id_i)
+        
+        if inOrOut == 'in':
+            intersection.addStreetI(street)
+        elif inOrOut == 'out':
+            intersection.addStreetO(street)
+        # updating or inserting the intersection
+        self.intersections[id_i] = intersection
 
     def readFile(self, path):
         hashcodeFile = open(path, 'r')
@@ -72,13 +103,13 @@ class Env:
             if (count > 1):
                 # Iterate on each street lines
                 if (self.S + 1 >= count):
-                    [id_b, end_e, name, time] = line.split() # read street line
-                    street = Street(id_b, end_e, name, time) # create new Street Object
-                    self.streets.append(street)
-                    # intersections_dict.setdefault(end_e,[]).append(street) # append street in it's endding intersection
-                    # print(end_e)
-                    # self.intersections[end_e] += [street] # append street in it's endding intersection TODO self.addIntersection (id, street, 'in/out')
-                    # street.print() # debug to print street infos
+                    [id_b, id_e, name, time] = line.split() # read street line
+                    street = Street(id_b, id_e, name, time) # create new Street Object
+                    self.streets.append(street) # adding our street in the env list
+
+                    self.addOrCreateIntersection(id_b, street, 'out') # register this street in the outgoing street list of the intersection id_b
+                    self.addOrCreateIntersection(id_e, street, 'in') # register this street in the incomming street list of the intersection id_e
+
                 elif (self.S + 1 + self.V >= count):
                     vehicle_infos = line.split() # read street line
                     vehicle = Vehicle(vehicle_infos[0], vehicle_infos[1:]) # create a new Vehicle
@@ -93,10 +124,12 @@ class Env:
         # print(intersections_dict)
         hashcodeFile.close()
 
+
 def main():
     env = Env()
     env.readFile('hashcode.in')
     print(f"D={env.D} I={env.I} S={env.S} V={env.V} F={env.F}")
+    print(env.intersections)
     
 
 if __name__ == "__main__":
